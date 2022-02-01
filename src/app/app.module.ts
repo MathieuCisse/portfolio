f4aba74408f96466b5754fdbfba165fec190d47a
Mathieu Cissé <tieutieu.mc@gmail.com>
2022-02-01 01:32:45 +0100

    Routing + Angular Material + Navbar + Début Home Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'training', component: HomeComponent },
+      {path: 'skills', component: HomeComponent },
+      {path: 'languages', component: HomeComponent },
+      {path: 'contact', component: HomeComponent },
+      {path: '**', redirectTo: '', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
